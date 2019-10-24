63b3d93e08773f76982e266bfca13ebcc37d0a7f
namanparashar123 <naman5770@gmail.com>
2019-10-24 10:57:33 -0500

    Updated routes and added maps.

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,10 @@
+class SearchController < ApplicationController
+  def index
+  end
+
+  def results
+  end
+
+  def violator_results
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'search/index'
+  get 'search/results'
+  get 'search/violator_results'
   get 'about/index'
   get 'violators/index'
   get 'violators/show'
   get 'tickets/index'
   get 'tickets/show'
+
+  resources :tickets do
+    resources :violators
+  end
+
+  resources :violators do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
